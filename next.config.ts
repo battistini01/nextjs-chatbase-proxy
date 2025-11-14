6816f0cd593a4ee38ac5cae9853c7658be200aa1
battistini01 <battwork@gmail.com>
2025-11-14 22:51:16 +0100

    test proxy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   /* config options here */
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'https://eed2d50e7a37.ngrok-free.app'],
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
   async rewrites() {
     return [
       {
